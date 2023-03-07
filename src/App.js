c110653b4f14b6133b64b58274582c11db8c7ca8
nizamuddin15bd <nizamuddin15bd@gmail.com>
2023-03-07 19:02:32 +0600

    module 56-6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { useState } from 'react';
 
 
 const auth = getAuth(app);
 function App() {
+  const [user, setUsers] = useState({})
   const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then(result => {
         const user = result.user;
+        setUsers(user);
         console.log(user);
       })
       .catch(error => {
         console.log('error', error);
       })
   }
+  const handleSingOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUsers({})
+      })
+      .catch(() => {
+        setUsers({})
+      })
+  }
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {
+        user.email ? <button onClick={handleSingOut}>Sing Out</button> :
+          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+
+      }
+      <h2>Name: {user.displayName}</h2>
+      <p>I know Your email: {user.email}</p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
--- src/_steps.js
+++ src/_steps.js
@@ -1,13 +1,14 @@
 /**
  * Steps to use firebase
  * 1.Create a project on console.firebase.google.com
- * 2.install firebase
+ * 2.npm install firebase
  * 3.Register Web app in firebase 
  * 4.copy firebase init with config from firebase project settings into a file firebase.init .js
- * 5.export default app
- * 6.import app firebase init.js into your app.js
- * 7.import getAuth from firebase/auth and create auth = getAuth(app)
+ * 5.export default app from firebase.init.js
+ * 6.import getAuth from firebase/auth and create const auth = getAuth(app) in app.js
+ * 7.import app firebase init.js into your app.js
  * 8.turn on google authentication(firebase >authentication>google sign in)
  * 9.crate google provider
- * 10.
+ * 10.use signInWithGooglePopup and pass auth and provider
+ * 11.handle .then (if successful) and catch error(if error)
  **/
